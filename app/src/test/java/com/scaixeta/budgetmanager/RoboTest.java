45077c09d12f32927c210f8d9e88ba13ee2bd72d
Sarah Caixeta <sarahc@LAscaixeta.local>
2014-11-09 23:58:59 -0200

    Project structure and set up for tests with robolectric

---
+++ app/src/test/java/com/scaixeta/budgetmanager/RoboTest.java
@@ -0,0 +1,15 @@
+package com.scaixeta.budgetmanager;
+
+import org.robolectric.annotation.Config;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import static org.junit.Assert.*;
+
+@Config(emulateSdk = 18)
+@RunWith(RobolectricTestRunner.class)
+public class RoboTest {
+    @Test public void testTrueIsTrue() throws Exception {
+        assertEquals(true, true);
+    }
+}
