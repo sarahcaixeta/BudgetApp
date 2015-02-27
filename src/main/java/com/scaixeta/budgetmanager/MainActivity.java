2e72c015195e1ae093a84c391a2a6233e49dfbb6
Sarah Caixeta <caixeta.sarah@gmail.com>
2015-02-27 18:09:03 -0300

    Adds a dummy  menu

--- src/main/java/com/scaixeta/budgetmanager/MainActivity.java
+++ src/main/java/com/scaixeta/budgetmanager/MainActivity.java
@@ -2,11 +2,15 @@ package com.scaixeta.budgetmanager;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.scaixeta.budgetmanager.fragments.BudgetSetupFragment;
 import com.scaixeta.budgetmanager.data.Budget;
+import com.scaixeta.budgetmanager.fragments.BudgetSetupFragment;
 
 import javax.inject.Inject;
 
@@ -37,5 +41,21 @@ public class MainActivity extends FragmentActivity implements BudgetSetupFragmen
         double dailyBudget = budgetCalculator.calculateDailyBudget(budget);
         resultText.setText(getResources().getString(R.string.budget_per_day, dailyBudget));
         findViewById(R.id.daily_budget_view).setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_new_expense){
+            Toast.makeText(this, "An expense should be created", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
